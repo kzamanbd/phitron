5c6d97bf3d7b4f04854071ace60535ff615b895b
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-10-13 00:01:28 +0600

    added assignment 02

---
+++ assignment-02/Is_It_a_Challenge.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, i;
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        for (i = 1; i <= n; i++)
+        {
+            printf("%d ", i);
+        }
+    }
+    else
+    {
+        for (i = n; i <= 0; i++)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
---
+++ assignment-02/Update_and_Print.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, i, position, value;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d %d", &position, &value);
+
+    arr[position] = value;
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
