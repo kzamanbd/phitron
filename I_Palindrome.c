d465ec89121e1d6c3a3e8aefc39a7605029fba09
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-10-15 00:26:58 +0600

    Pattern-Programs-in-C added

--- I_Palindrome.c
+++ I_Palindrome.c
@@ -3,8 +3,26 @@
 int main()
 {
 
-    int n;
+    int n, reverse = 0, reminder;
     scanf("%d", &n);
+
+    int copyNum = n;
+    while (copyNum != 0)
+    {
+        reminder = copyNum % 10;
+        reverse = reverse * 10 + reminder;
+        copyNum /= 10;
+        // printf("%d %d %d\n", reminder, reverse, copyNum);
+    }
+
+    if (n == reverse)
+    {
+        printf("%d\nYES", reverse);
+    }
+    else
+    {
+        printf("%d\nNO", reverse);
+    }
 
     return 0;
 }
